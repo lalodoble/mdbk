b6beaa1f42266feb50d1bb04b0604dc2495b2f23
lalo.01.g@gmail.com <lalo.01.g@gmail.com>
2022-01-31 19:57:09 -0300

    finished

--- contact.php
+++ contact.php
@@ -1,12 +1,19 @@
 <?php
-$to = 'lalo.01.g@gmail.com';
+$to = 'info@mdbk.com.ar';
 
 $name 	= $_POST['name'];
 $email 	= $_POST['email'];
 $tel 	= $_POST['tel'];
 $msg 	= $_POST['msg'];
 
-mail($to, "Mensaje Web de " . $name, $msg);
+$final_msg = "mdbk.com.ar - Mensaje Web \n\n";
+$final_msg .= "========================================= \n";
+$final_msg .= "========================================= \n\n";
+$final_msg .= "Nombre: " . $name ."\n\n";
+$final_msg .= "Email: " . $email ."\n\n";
+$final_msg .= "Teléfono: " . $tel ."\n\n";
+$final_msg .= "Mensaje: " . $msg ."\n\n";
+
+mail($to, "mdbk.com.ar - Mensaje Web", $final_msg);
 
 header('location: contact-ok.html');
-?>
